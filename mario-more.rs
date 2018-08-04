58127aff34378ab52efaf1843a22e0429bc6c8cc
sabelo <ntabenism@gmail.com>
2018-08-04 17:43:09 +0200

    mario more

---
+++ mario-more.rs
@@ -0,0 +1,43 @@
+// The Rust implementation of cs50 pset1 mario
+// Student: Sabelo Ntabeni
+// CS50 2018
+// Instructor: David J Malan
+// pset1/mario/more
+use std::io;
+
+fn main() {
+    let n: i32;
+    loop {
+        println!("Height:");
+        let mut height = String::new();
+        io::stdin().read_line(&mut height).expect("Failed");
+        let height: i32 = match height.trim().parse() {
+            Ok(ht) => ht,
+            Err(_) => continue,
+        };  
+        if !(height < 0 || height > 23) { n = height; break;} 
+    }
+    // drawing pyramid
+    // Rnth row is defined :
+    //  R = n(height - n)(spaces) + n(blocks) + 2spaces + n(blocks) + line break
+    // let r = n , for n>0
+    for row in 1..=n {
+        //draw left half of pyramid
+        // padding from left edge
+        for _left_spaces in 0..(n - row) {
+            print!("{}",' ');
+        }
+        // left set of blocks 
+        for _blocks in 0..row {
+            print!("#");
+        }
+        // gap between left and right pyramid halves
+        print!("  ");
+        // right pyramid half
+        for _blocks in 0..row {
+            print!("#");
+        }
+        // pyramid level finished
+        println!(); 
+    }
+} 
