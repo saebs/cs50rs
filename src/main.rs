3ea92a5779107f0cb47de2ab5cc68e5d95cc9d9e
saebs <ntabenism@gmail.com>
2018-11-15 16:07:03 +0200

    organising logic into library

--- src/main.rs
+++ src/main.rs
@@ -5,41 +5,10 @@
 */
 // The main.rs file is custom, commandline arguments will invoke the required module or pset to run
 // for psets with further arguments , inputs start from argument three ie index 2
-mod pset1_rust; // hello, mario(more comfortable), credit
-mod pset2_crypto; // caesar, crack(pending calling c code lesson)
-
-// Problem sets
-use pset1_rust::*; // aka C
-use pset2_crypto::*; 
 // other 
-use std::env;
-use std::process;
-use std::io::Write;
-
+extern crate cs50rs;
 fn main() {
-    let cs50: Vec<String> = env::args().collect();
-    if cs50.len() > 1 {
-        let  pset = &cs50[1].to_lowercase();
-        match pset.trim() {
-            // psets to run
-            "hello" => {intro(&cs50); hello::play()}, 
-            "mario" => {intro(&cs50); mario_more::play()}, 
-            "caesar" => {intro(&cs50); caesar::play()}, 
-            "credit" => {intro(&cs50); credit::play()},
-            "crack" => {intro(&cs50); crack::play()},
-            "vigenere" =>{intro(&cs50); vigenere::play()}, 
-            _ => { writeln!(std::io::stderr(),"see README.md file for list of pset names \n https://github.com/saebs/cs50rs/blob/master/README.md").unwrap(); process::exit(1)}, //,
-        } 
-    } else{
-        writeln!(std::io::stderr(), r##"Usage: cs50rs <pset name> 
-"see README.md file for list of pset names"##).unwrap();
-    }
-
-// helper function
-
-fn intro(cs: &Vec<String>){
-    println!("\n\u{25b6}  {}" , cs[1].to_uppercase()); // play button
-}
+    cs50rs::run();
 }
 
 //ZOU: APL189395
