b5549797a13f70df0c592a33ccfa53ec183d371c
Sabelo Ntabeni <saebs@pop-os.localdomain>
2018-11-16 13:14:14 +0200

    refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -3,34 +3,21 @@ mod pset2_crypto;
 
 use pset1_rust::*; // aka C
 use pset2_crypto::*; 
-use std::env;
 use std::process;
 use std::io::Write;
 
-pub fn run() {
-    let cs50: Vec<String> = env::args().collect();
-    if cs50.len() > 1 {
-        let  pset = &cs50[1].to_lowercase();
+pub fn run(query: &Vec<String>) {
+        let  pset = &query[1].to_lowercase();
         match pset.trim() {
             // psets to run
-            "hello" => {intro(&cs50); hello::play()}, 
-            "mario" => {intro(&cs50); mario_more::play()}, 
-            "caesar" => {intro(&cs50); caesar::play()}, 
-            "credit" => {intro(&cs50); credit::play()},
-            "crack" => {intro(&cs50); crack::play()},
-            "vigenere" =>{intro(&cs50); vigenere::play()}, 
-            _ => { writeln!(std::io::stderr(),"see README.md file for list of pset names \n https://github.com/saebs/cs50rs/blob/master/README.md").unwrap(); process::exit(1)}, //,
+            "hello" => hello::play(), 
+            "mario" => mario_more::play(), 
+            "caesar" => caesar::play(), 
+            "credit" => credit::play(),
+            "crack" => crack::play(),
+            "vigenere" => vigenere::play(), 
+            _  => { writeln!(std::io::stderr(),"Something went wrong Or pset not implemented yet!!").unwrap(); process::exit(1)}, //,
         } 
-    } else{
-        writeln!(std::io::stderr(), r##"Usage: cs50rs <pset name> 
-"see README.md file for list of pset names"##).unwrap();
-    }
-
-// helper function
-
-fn intro(cs: &Vec<String>){
-    println!("\n\u{25b6}  {}" , cs[1].to_uppercase()); // play button
-}
 }
 
 //ZOU: APL189395
--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,35 @@
 // for psets with further arguments , inputs start from argument three ie index 2
 // other 
 extern crate cs50rs;
+use std::env;
+use cs50rs::run;
+use std::io::Write;
+use std::process;
 fn main() {
-    cs50rs::run();
+    let cs50: Vec<String> = env::args().collect();
+    if cs50.len() < 2 {
+        help();
+    } else {
+        let args = &cs50[1].to_lowercase();
+        if args.trim() != "help" {
+            println!("\u{25b6}\n{} running\n", &cs50[1].to_uppercase());
+            run(&cs50);
+        }
+    } 
 }
 
+fn help() {
+             writeln!(std::io::stderr(),
+r##"Usage.. cs50rs "pset name"
+
+[Options]
+"hello" 
+"mario" 
+"caesar" 
+"credit" 
+"crack" 
+"vigenere"
+"##).unwrap(); process::exit(1) 
+
+}
 //ZOU: APL189395
