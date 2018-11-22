8a5c9cd2fdced19966b0cdef08b1996790159ee2
saebs <sabelo.n@yandex.com>
2018-11-22 12:05:57 +0200

    fix error calling c

--- src/pset2_crypto/crack.rs
+++ src/pset2_crypto/crack.rs
@@ -8,10 +8,10 @@ extern "C" {
 
 
 pub fn play() {
-unsafe {
+/* unsafe {
     let ciphertext = crypt('a', 50); // fdup , I'll be back when i have a black belt
     println!("{:?}", ciphertext);
-}
+} */
 let alphabet = vec!["a","b","c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 let mut gen_word = String::new();
     for &first in alphabet.iter() {
